e67991dda282e347e673d06bc956264b0f4693ad
Rajesh Pandian M <mrpraj@gmail.com>
2022-11-18 14:15:04 +0530

    stl iota edited

--- cpp/stl/iota.cpp
+++ cpp/stl/iota.cpp
@@ -8,7 +8,7 @@ void print(const vector<int>& a){
 int main(int argc, char* argv[]){
   vector<int> a(10);
   // Third arg is a must. It is begin of the sequence.
-  std::iota(a.begin(),a.end(),0);  print(a);  // 0 1 2 3 4 5 6 7 8 9 
-  std::iota(a.begin(),a.end(),20); print(a);  //20 21 22 23 24 25 26 27 28 29
+  std::iota(a.begin(),a.end(),0);  print(a);  //  0  1  2  3  4  5  6  7  8  9 
+  std::iota(a.begin(),a.end(),20); print(a);  // 20 21 22 23 24 25 26 27 28 29
   return 0;
 }
